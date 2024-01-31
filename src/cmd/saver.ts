41c1914baf813a997937b45ee688c45a24f93ee1
Abiria <abiria.dev@gmail.com>
2024-01-31 18:13:09 +0900

    feat: update saver to print to stdout if the given path is dash

--- src/cmd/saver.ts
+++ src/cmd/saver.ts
@@ -1,7 +1,13 @@
 import { writeFile } from 'node:fs/promises'
 import { fetchJson } from '@/index.js'
+import { argv, stdout } from 'node:process'
 
-await writeFile(
-	'./colors.json',
-	JSON.stringify(await fetchJson(), null, 4),
-)
+const res = JSON.stringify(await fetchJson(), null, 4)
+
+const outFile = argv[2] ?? './colors.json'
+
+if (outFile === '-') {
+	stdout.write(res)
+} else {
+	writeFile(outFile, res)
+}
